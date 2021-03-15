7f471c58d6f1cdc15177644075c98ade164635ca
Michał Dering <michal.dering@globallogic.com>
2021-03-15 13:34:51 +0100

    Slight changes

--- validation/tasks.js
+++ validation/tasks.js
@@ -9,28 +9,24 @@ module.exports = {
 			let statusCode = 400;
 			let message = 'no userId supplied or not as a number';
 			res.status(statusCode);
-			console.log(message);
 			return { isError, statusCode, message }
 		} else if (!body.summary) {
 			let isError = true;
 			let statusCode = 400;
 			let message = 'no summary supplied';
 			res.status(statusCode);
-			console.log(message);
 			return { isError, statusCode, message }
 		} else if (!body.status) {
 			let isError = true;
 			let statusCode = 400;
 			let message = 'no status supplied';
 			res.status(statusCode);
-			console.log(message);
 			return { isError, statusCode, message }
 		} else if (!config.taskStatuses.includes(body.status)) {
 			let isError = true;
 			let statusCode = 400;
 			let message = 'Wrong status supplied. Possible values: ' + config.taskStatuses;
 			res.status(statusCode);
-			console.log(message);
 			return { isError, statusCode, message }
 		}  else {
 			let isError = false;
--- validation/worklogs.js
+++ validation/worklogs.js
@@ -8,21 +8,18 @@ module.exports = {
 			let statusCode = 400;
 			let message = 'no userId supplied or not as a number';
 			res.status(statusCode);
-			console.log(message);
 			return { isError, statusCode, message }
 		} else if (!body.taskId || isNaN(body.taskId)) {
 			let isError = true;
 			let statusCode = 400;
 			let message = 'no taskId supplied or not as a number';
 			res.status(statusCode);
-			console.log(message);
 			return { isError, statusCode, message }
 		} else if (!body.reportedHours || isNaN(body.reportedHours)) {
 			let isError = true;
 			let statusCode = 400;
 			let message = 'no reportedHours supplied or not as a number';
 			res.status(statusCode);
-			console.log(message);
 			return { isError, statusCode, message }
 		} else {
 			let isError = false;
